1ccece4434472dd5ebcb5a9db92107829d06af0d
MubeenShaikh123 <106340769+MubeenShaikh123@users.noreply.github.com>
2023-12-21 16:56:22 +0530

    Update controller.js

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -150,7 +150,6 @@ exports.sendOtp = async (req, res) => {
   }
   
   const userMail = req.body.email;
-  console.log("userMail",userMail)
   const otp = String(Math.floor(Math.random() * 9000) + 1000);
 
   const transporter = nodemailer.createTransport({
@@ -171,25 +170,20 @@ exports.sendOtp = async (req, res) => {
   // Check if a user with the provided email exists in the database
   User.findOne({ email: userMail })
     .then((existingUser) => {
-      console.log("0th then start")
       if (existingUser) {
-        console.log("0th then if")
         // User exists, continue with OTP operations
         return Otp.findOne({ email: userMail });
       } else {
-        console.log("0th then else")
         // User does not exist, return an error
         return Promise.reject('User not found');
       }
     })
     .then((existingOtp) => {
-      console.log("first then start")
       if (existingOtp) {
         // OTP exists, update it
         existingOtp.otp = otp;
         existingOtp.used = false;
         existingOtp.date = new Date();
-        console.log("first then if")
         return existingOtp.save();
       } else {
         // OTP does not exist, create a new record
@@ -198,32 +192,25 @@ exports.sendOtp = async (req, res) => {
           otp: otp,
           used: false,
         });
-        console.log("first then else")
         return newOtp.save();
       }
     })
     .then(() => {
       // Send the email
-      console.log("second then start")
       return new Promise((resolve, reject) => {
         transporter.sendMail(mailOptions, (error, info) => {
           if (error) {
-            console.log("second then if")
             reject(error);
           } else {
-            console.log("second then else")
             resolve(info);
           }
         });
       });
     })
     .then((info) => {
-      console.log("third then start")
       res.json({ message: 'OTP Sent Successfully' });
     })
     .catch((error) => {
-      console.log("catch start")
-      console.log("error",error)
       if (error === 'User not found') {
         res.status(404).json({ error: [{ message: 'User not found' }] });
       } else {
@@ -359,9 +346,6 @@ exports.verifyOtp = async (req, res) => {
 
 exports.changePassword = async (req, res) => {
   const { email, password, confirmPassword } = req.body;
-console.log("email",email)
-console.log("password",password)
-console.log("confirmPassword",confirmPassword)
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     const errorResponse = [];
